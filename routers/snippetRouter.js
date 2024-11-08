8f1814415a38734ce707a3e229530006f356f742
Ran Helfer <rhelfer@gmail.com>
2024-11-08 11:17:47 +0200

    adding delete and put - CRUD is working

--- routers/snippetRouter.js
+++ routers/snippetRouter.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
 const Snippet = require("../models/snippetModel");
 
-router.get("/test", (req, res) => {
-    res.send("Router test")
+router.get("/", async (req, res) => {
+    try {
+        const snippets = await Snippet.find();
+        res.json(snippets)
+    } catch (err) {
+        console.log({err})
+        return res.status(500).json({
+            error: "An error occurred"
+        })
+    }
 });
 
 router.post("/", async (req, res) => {
@@ -17,7 +25,7 @@ router.post("/", async (req, res) => {
     if(!description && !code) {
         return res.status(400).json({
              errorMessage: "You need to enter description or some code"
-        })
+        });
     }
     
     console.log("title is --> " + title);
@@ -32,13 +40,93 @@ router.post("/", async (req, res) => {
     try {
         const savedSnippet = await newSnippet.save();
 
-        res.send(savedSnippet)
+        res.send(savedSnippet);
     } catch (err) {
         console.log({err})
         return res.status(500).json({
             // error: err.message || "An error occurred" // You do not want to return the actual error
             error: "An error occurred"
-        })
+        });
+    }
+});
+
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const snippetId = req.params.id;
+
+        // Validation
+
+        if(!snippetId) {
+            return res.status(400).json({
+                 errorMessage: "No deletion id"
+            });
+        }
+
+        const existingSnippet = await Snippet.findById(snippetId);
+
+        if(!existingSnippet) {
+            return res.status(400).json({
+                 errorMessage: "No existing record for such id"
+            });
+        }
+
+        await existingSnippet.deleteOne();
+        //await existingSnippet.delete();
+
+        console.log(existingSnippet);
+        res.json(existingSnippet);
+    } catch (err) {
+        console.log({err})
+        return res.status(500).json({
+            error: "An error occurred"
+        });
+    }
+});
+
+
+router.put("/:id", async (req, res) => {
+    try {
+        const snippetId = req.params.id;
+        const {title, description, code} = req.body;
+
+        // Validation
+
+        if(!snippetId) {
+            return res.status(400).json({
+                 errorMessage: "No update id"
+            });
+        }
+
+        // Validation
+
+        if(!description && !code) {
+            return res.status(400).json({
+                 errorMessage: "You need to enter description or some code"
+            });
+        }
+
+        const existingSnippet = await Snippet.findById(snippetId);
+
+        if(!existingSnippet) {
+            return res.status(400).json({
+                 errorMessage: "No existing record for such id"
+            });
+        }
+
+        existingSnippet.title = title;
+        existingSnippet.code = code;
+        existingSnippet.description = description;
+        
+        await existingSnippet.save();
+
+        console.log(existingSnippet);
+        res.json(existingSnippet);
+    } catch (err) {
+        console.log({err})
+        return res.status(500).json({
+            error: "An error occurred"
+        });
     }
 });
 
